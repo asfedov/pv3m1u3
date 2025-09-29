cb02f0f24ff1f425dea727ae2ed25bbf5e6e6b15
ahiehe <ahiehe@outlook.com>
2025-09-29 11:08:44 +0200

    added acceptance handler

--- bot.py
+++ bot.py
@@ -30,4 +30,11 @@ def moderate_message(message):
         bot.reply_to(message, f"Пользователь @{message.from_user.username} был забанен.")
     bot.reply_to(message, message.text) 
 
+
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
+    
 bot.infinity_polling(none_stop=True)
